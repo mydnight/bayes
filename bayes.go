8dd29f01865eaab10035ab0ee344709d3a7be784
mydnight <mydnight@technomagia.tk>
2015-02-25 20:06:32 -0600

    Update bayes.go
    Major overhaul to correct underflow errors and separate into its own package.

--- bayes.go
+++ bayes.go
@@ -7,43 +7,28 @@ package bayes
  *it based on previously trained data.*/
 
 import (
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	"log"
 	"sync"
+	"math"
 )
 
-//Tolerance := 0.05 //Tolerance -- i.e. if the result is 0.5 +- tolerance value, return "undetermined" category
-
-type index struct {
+type Index struct {
 	sync.RWMutex
-	m map[string]float64
+	M map[string]float64
+	Name string
+	Total float64
 }
 
 //Classify takes a text sample and classifies it as one of two categories.
-func Classify(text []string, cat1Index string, cat2Index string, tolerance float64) int {
-	myCat1Index := loadIndex(cat1Index)
-	myCat2Index := loadIndex(cat2Index)
-	myCat1Index.RLock()
-	myCat2Index.RLock()
-	//iterate over goodindex and badindex, and sum the values.
-	cat1total := 0.00
-	cat2total := 0.00
-	for _, value := range myCat1Index.m {
-		cat1total += value
-	}
-	for _, value := range myCat2Index.m {
-		cat2total += value
-	}
+func Classify(text []string, cat1Index Index, cat2Index Index, tolerance float64) string {
+	cat1Index.RLock()
+	cat2Index.RLock()
 	i := 0.00
 	invi := 0.00
 	for _, token := range text {
-		cat1token := myCat1Index.m[token]
-		cat2token := myCat2Index.m[token]
-		prob := calcProbability(cat1token, cat1total, cat2token, cat2total)
-		if i != 0.00 {
+		prob := calcProbability(cat1Index.M[token], cat1Index.Total, cat2Index.M[token], cat2Index.Total)
+		if i > 0.00  && prob > 0.00{
 			i = i * prob
-		} else {
+		} else if i == 0.00 {
 			i = prob
 		}
 		if invi != 0.00 {
@@ -52,77 +37,36 @@ func Classify(text []string, cat1Index string, cat2Index string, tolerance float
 			invi = 1.00 - prob
 		}
 	}
-	myCat1Index.RUnlock()
-	myCat2Index.RUnlock()
+	cat1Index.RUnlock()
+	cat2Index.RUnlock()
 	totprob := i / (i + invi)
 	if totprob > (0.5 + tolerance) {
-		return 1
+		return cat1Index.Name 
 	} else if totprob < (0.5 - tolerance) {
-		return -1
+		return cat2Index.Name
 	}
-	return 0
+	return "Unknown"
 }
 
 //calcProbability calculates the probability any given word is part of either category
 func calcProbability(cat1count float64, cat1total float64, cat2count float64, cat2total float64) float64 {
-	bw := cat1count / cat1total
-	gw := cat2count / cat2total
-	pw := ((bw) / ((bw) + (gw)))
-	s := 1.00
-	x := 0.5
-	n := cat1count + cat2count
-	fw := ((s * x) + (n * pw)) / (s + n)
-	return fw
-
+	cat1Percent:= math.Log(cat1count / cat1total)
+	cat2Percent:= math.Log(cat2count / cat2total)
+	probWord := cat1Percent / (cat1Percent + cat2Percent)
+	if math.IsNaN(probWord) { //Word doesn't exist in either of the indices
+		probWord = 0.0
+	}
+	return probWord
 }
 
 //Train trains a category with a text sample.
-func Train(trainedIndex string, text []string) {
-	myIndex := loadIndex(trainedIndex)
+func Train(myIndex Index, text []string) Index {
+	//myIndex := loadIndex(trainedIndex)
 	myIndex.Lock()
 	for _, token := range text {
-		myIndex.m[token]++
+		myIndex.M[token]++
+		myIndex.Total++
 	}
-	saveIndex(trainedIndex, myIndex)
 	myIndex.Unlock()
-}
-
-//loadIndex loads an index from a database.
-func loadIndex(indexName string) index {
-
-	myIndex := index{}
-	database, err := sql.Open("sqlite3", "./bayes.db")
-	_, err = database.Exec(
-		"CREATE TABLE IF NOT EXISTS Bayes ( id varchar(255), token varchar(255), count REAL, PRIMARY KEY (id, token))")
-	if err != nil {
-		log.Fatal(err)
-	}
-	sth, err := database.Query("SELECT token, count FROM Bayes WHERE id=?", indexName)
-	myIndex.m = make(map[string]float64)
-	for sth.Next() {
-		var token sql.NullString
-		var count sql.NullFloat64
-		if err := sth.Scan(&token, &count); err != nil {
-			log.Fatal(err)
-		}
-		myCount := count.Float64
-		myToken := token.String
-		myIndex.m[myToken] = myCount
-	}
-	database.Close()
 	return myIndex
-	//load index from database
-}
-
-//saveIndex saves the index to the database.
-func saveIndex(indexName string, myIndex index) {
-	//save index to database
-	database, err := sql.Open("sqlite3", "./rss2.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for token, count := range myIndex.m {
-		database.Exec("INSERT OR REPLACE INTO Bayes(id, token, count) VALUES(?,?,?)", indexName, token, count)
-	}
-	database.Close()
 }
